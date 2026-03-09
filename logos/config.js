958aec9c9a03f9d621dfd6438a12d22f5b78154c
luisho24 <19247810+luisho24@users.noreply.github.com>
2026-03-09 15:46:50 -0500

    included all carrier logos

---
+++ logos/config.js
@@ -0,0 +1,14 @@
+{
+  "isActive": true,
+  "killSwitchMessage": "⚠️ La extensión está suspendida temporalmente por mantenimiento de Priority1.",
+  "announcement": {
+    "show": true,
+    "message": "🔥 Bienvenido a P1 Smart Parser v2.0 - Batch Mode Activado",
+    "type": "info"
+  },
+  "features": {
+    "batchMode": true
+  },
+  "customIconsUrl": "https://raw.githubusercontent.com/luisho24/quotool/refs/heads/main/logos/"
+}
+
