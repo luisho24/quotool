ebd3452e657890672709f91d1e97c08a40ffec61
luisho24 <19247810+luisho24@users.noreply.github.com>
2026-03-09 16:15:05 -0500

    updated config

--- config.js
+++ config.js
@@ -3,7 +3,7 @@
   "killSwitchMessage": "⚠️ La extensión está suspendida temporalmente por mantenimiento de Priority1.",
   "announcement": {
     "show": true,
-    "message": "🔥 Welcome to Smart Quote Parser tool",
+    "message": "PRUEBA EN VIVO",
     "type": "info"
   },
   "features": {
@@ -11,4 +11,3 @@
   },
   "customIconsUrl": "https://raw.githubusercontent.com/luisho24/quotool/refs/heads/main/logos/"
 }
-
